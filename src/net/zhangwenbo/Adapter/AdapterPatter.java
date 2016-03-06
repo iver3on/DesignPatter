696cea0da2885b34b9b5c361614557206bde7bb5
iver333 <316397827@qq.com>
2016-03-06 16:21:34 +0800

    add AdapterOfInterface

--- src/net/zhangwenbo/Adapter/AdapterPatter.java
+++ src/net/zhangwenbo/Adapter/AdapterPatter.java
@@ -1,5 +1,9 @@
 package net.zhangwenbo.Adapter;
 
-public class AdapterPatter {
+public class AdapterPatter  extends Source implements Targetable{
 
+	@Override
+	public void method2() {
+		   System.out.println("this is the targetable method!");  	
+	}		
 }
---
+++ src/net/zhangwenbo/AdapterOfInterface/Wrapper.java
@@ -0,0 +1,12 @@
+package net.zhangwenbo.AdapterOfInterface;
+
+public abstract class Wrapper implements Sourceable {
+
+	@Override
+	public void method1() {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public void method2() {
+	}
+}
